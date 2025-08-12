519fb15d00d75747c7b1f00a51e9af004c8921cf
Артем <kotbegemot001@yandex.ru>
2025-08-12 13:57:16 +0500

    Update spentcalories.go

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -6,13 +6,12 @@ import (
 	"strings"
 	"time"
 )
-
 const (
-	lenStep                    = 0.65
+	lenStep                    = 0.65 
 	mInKm                      = 1000 
-	minInH                     = 60  
-	stepLengthCoefficient      = 0.45
-	walkingCaloriesCoefficient = 0.5  
+	minInH                     = 60 
+	stepLengthCoefficient      = 0.45 
+	walkingCaloriesCoefficient = 0.5 
 	runningCaloriesCoefficient = 0.029 
 )
 
