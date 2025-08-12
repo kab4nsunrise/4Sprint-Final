01f97acc23583b662ea0221ae7f66094d01eda17
Артем <kotbegemot001@yandex.ru>
2025-08-12 13:39:06 +0500

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,10 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,9 +16,62 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, errors.New("неверный формат количества шагов")
+	}
+
+	steps, err := strconv.Atoi(strings.TrimSpace(parts[0]))
+	if err != nil {
+		return 0, 0, errors.New("неверный формат количества шагов")
+	}
+	if steps <= 0 {
+		return 0, 0, errors.New("количество шагов должно быть положительным")
+	}
+
+	durationStr := strings.TrimSpace(parts[1])
+	if !strings.Contains(durationStr, "s") && !strings.Contains(durationStr, "m") && !strings.Contains(durationStr, "h") {
+		return 0, 0, errors.New("неверный формат продолжительности")
+	}
+
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		duration, err = time.ParseDuration(durationStr + "0s")
+		if err != nil {
+			return 0, 0, errors.New("неверный формат продолжительности")
+		}
+	}
+
+	return steps, duration, nil
 }
 
-func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+func DayActionInfo(data string, weight, height float64) (string, error) {
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		return "", fmt.Errorf("ошибка разбора данных: %w", err)
+	}
+
+	sc := NewSpentCalories()
+	distanceKm := sc.Distance(steps, height)
+	
+	var calories float64
+	if duration.Minutes() < 30 {
+		calories, err = sc.RunningCalories(steps, weight, height, duration)
+	} else {
+		calories, err = sc.WalkingCalories(steps, weight, height, duration)
+	}
+	
+	if err != nil {
+		return "", fmt.Errorf("ошибка расчета калорий: %w", err)
+	}
+
+	return formatDayActionInfo(steps, distanceKm, calories), nil
+}
+
+func formatDayActionInfo(steps int, distance, calories float64) string {
+	return strings.Join([]string{
+		"Количество шагов: " + strconv.Itoa(steps) + ".",
+		"Дистанция составила " + strconv.FormatFloat(distance, 'f', 2, 64) + " км.",
+		"Вы сожгли " + strconv.FormatFloat(calories, 'f', 2, 64) + " ккал.",
+	}, "\n")
 }
